4e87396accc9551d518aa0bf04e1cd110c7c5abc
Yegor Kandakov <yegorkandakov@gmail.com>
2018-04-25 17:08:08 +0300

    displaying new events on calendar page

--- src/routes/event.js
+++ src/routes/event.js
@@ -6,12 +6,15 @@ import authenticate from "../middlewares/authenticate";
 const router = express.Router();
 router.use(authenticate);
 
+router.get("/", (req, res) => {
+  Event.find({userId: req.currentUser._id}).then(events => res.json({events}));
+});
+
 router.post("/add", (req, res) => {
-  const {start, duration, title} = req.body.event;
-  const userId = req.currentUser._id;
-  const event = new Event({start, duration, title, userId});
-  event.save()
-  .catch(err => res.status(400).json({errors: parseErrors(err.errors)}));
+  Event.create({ start: req.body.event.start, duration: req.body.event.duration,
+    title: req.body.event.title, userId: req.currentUser._id})
+    .then(event => res.json({ event }))
+    .catch(err => res.status(400).json({errors: parseErrors(err.errors)}));
 });
 
 export default router;
